0015914184ef876e45e3a1e059337a99719d0175
Gikubu <davidndungu8203@gmail.com>
2024-03-26 14:00:05 +0300

    this creates a class for the student,a constructor and a method to print student information.

--- bin/classandteacher.dart
+++ bin/classandteacher.dart
@@ -1,1 +1,19 @@
+// Defines a class for Student
+class Student {
+  String name; // Name of the student
+  int age; // Age of the student
+  String gradeLevel; // Grade level of the student
+
+  // Constructor for Student class
+  Student(this.name, this.age, this.gradeLevel);
+
+  // Method to print student information
+  void printInfo() {
+    print('Student Information:');
+    print('Name: $name');
+    print('Age: $age');
+    print('Grade Level: $gradeLevel');
+  }
+}
+
 void main(List<String> arguments) {}
